d4f33165395018b64f5008a51e958eabbbe7661e
Furnok <killian.furno@orange.fr>
2025-02-03 10:22:03 +0100

    Feat: Add ScriptableObject Score Win

---
+++ Assets/Game/Scripts/SSO/SSO_ScoreWin.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SSO_ScoreWin", menuName = "Data/SSO/Game/ScoreWin")]
+public class SSO_ScoreWin : BT.ScriptablesObject.StaticScriptableObject<int>{}
