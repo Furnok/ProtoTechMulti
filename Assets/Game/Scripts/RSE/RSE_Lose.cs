951094e9d4c240d3096ff87fb22c77fa03b68708
Furnok <killian.furno@orange.fr>
2025-02-03 10:00:38 +0100

    Feat: More ScriptableObjects Preparation

---
+++ Assets/Game/Scripts/RSE/RSE_Lose.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_Lose", menuName = "Data/RSE/Game/Lose")]
+public class RSE_Lose : BT.ScriptablesObject.RuntimeScriptableEvent{}
---
+++ Assets/Game/Scripts/RSE/RSE_Win.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_Win", menuName = "Data/RSE/Game/Win")]
+public class RSE_Win : BT.ScriptablesObject.RuntimeScriptableEvent{}
